966134f3897affa9b1e06fbccb9ed5bda5be38b9
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-23 20:08:12 +0800

    feat(post): adds uppercase alias

--- lib/commands/post.ts
+++ lib/commands/post.ts
@@ -2,8 +2,11 @@ import { Command } from "@commander-js/extra-typings";
 
 const program = new Command();
 
-const post = program.command("post").action(() => {
-  console.log("post");
-});
+const post = program
+  .command("post")
+  .alias("POST")
+  .action(() => {
+    console.log("post");
+  });
 
 export default post;
