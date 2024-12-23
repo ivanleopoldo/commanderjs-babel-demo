e89568eaf0f43f388d138da585d5d3df9ba19c8d
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-23 22:12:15 +0800

    refactor: placed main run function to core folder

---
+++ lib/core/run.ts
@@ -0,0 +1,28 @@
+import instance from "axios";
+import * as readline from "node:readline/promises";
+import { handleError } from "@/utils/error-handler";
+
+export async function run(baseURL: string) {
+  const axios = instance.create({ baseURL: baseURL });
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  while (true) {
+    const request = await rl.question("");
+
+    if (request) {
+      try {
+        const res = await axios(request);
+        console.log(res.data);
+      } catch (err) {
+        handleError(err);
+      }
+    } else {
+      break;
+    }
+  }
+
+  rl.close();
+}
