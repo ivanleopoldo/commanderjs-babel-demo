c22906095556f20300363d4202be3a581adbc3c6
ivanleopoldo <ivanleopoldoc@gmail.com>
2024-12-23 20:33:02 +0800

    feat: adds subcommands to parent; url validate checking

--- lib/index.ts
+++ lib/index.ts
@@ -2,21 +2,35 @@
 
 import { Argument, Command } from "@commander-js/extra-typings";
 import { post } from "./commands";
+import get from "./commands/get";
+import put from "./commands/put";
+import del from "./commands/delete";
 
 const program = new Command();
 
 program
-  .description("parent desc")
-  .addArgument(new Argument("[name]"))
+  .description("A pipeline-based API testing CLI tool")
+  .addArgument(new Argument("[url]", "Base API URL"))
   .hook("preAction", (com) => {
     if (com.args.length === 0) {
       com.help();
     }
   })
-  .action((name) => {
-    console.log("parent ", name);
+  .action((url) => {
+    if (!url) {
+      throw Error("URL is required");
+    }
+
+    try {
+      const urlObj = new URL(url);
+    } catch (err) {
+      console.error("Must be a valid URL");
+    }
   });
 
 program.addCommand(post);
+program.addCommand(get);
+program.addCommand(put);
+program.addCommand(del);
 
 program.parse(process.argv);
