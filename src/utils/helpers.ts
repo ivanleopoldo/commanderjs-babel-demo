8f97a0c8975ee5eceb64d597f380299488991392
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-07 19:06:51 +0800

    feat: adds functionality to get command

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,7 +4,7 @@ export function requestSplitter(input: string): TFetchOptions {
   const trimmed = input.trim();
   const split = trimmed.split(" ");
 
-  const type = split.length === 1 ? "GET" : split[0].toUpperCase() as TMethod;
+  const type = split.length === 1 ? "GET" : (split[0].toUpperCase() as TMethod);
   const endpoint = split.length === 1 ? split[0] : split[1];
 
   return {
