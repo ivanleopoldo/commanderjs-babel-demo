53ef9ffad0bc45726a2027fd26753b504d73306f
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-07 21:41:28 +0800

    refactor(types): changes interfaces to types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,17 @@
 export type TMethod = "GET" | "POST" | "PUT" | "DELETE";
 
-export interface TFetchOptions {
+export type TFetchOptions = {
   type: TMethod;
   endpoint: string;
   headers?: Headers;
   body?: Record<string, any>;
-}
+};
 
-export interface TRunOptions {
+export type TServiceOptions = {
+  baseUrl?: string;
+};
+
+export type TRunOptions = {
   editor_command?: string;
   prompt?: string;
-}
+};
