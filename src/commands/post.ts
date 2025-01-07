e116b8f1358d385b5a0980a3edbed8fcde9ff9e2
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-01-07 19:10:45 +0800

    feat: adds post command functionality

--- src/commands/post.ts
+++ src/commands/post.ts
@@ -1,12 +1,28 @@
 import { Command } from "@commander-js/extra-typings";
+import axios from "axios";
+import { spawnSync } from "bun";
+import fs from "node:fs";
+import temp from "temp";
 
 const program = new Command();
 
 const post = program
   .command("post")
+  .argument("[url]", "URL to request")
   .alias("POST")
-  .action(() => {
-    console.log("post");
+  .action(async (url) => {
+    if (!url) throw Error("URL is required");
+
+    const tempFile = temp.openSync({ suffix: ".json" });
+
+    spawnSync(["nvim", tempFile.path], {
+      stdio: ["inherit", "inherit", "inherit"],
+    });
+
+    const output = fs.readFileSync(tempFile.path, "utf-8");
+    temp.cleanupSync();
+    const response = await axios.post(url, JSON.parse(output));
+    console.log(response.data);
   });
 
 export default post;
